99d06b53f531bf41b2cef5069db41039050e22f9
rishabh <rishabhv47@gmail.com>
2021-12-08 00:48:55 +0530

    [workation-03] [FEATURE] backend server setup tuesday

--- backend/authenticateToken.js
+++ backend/authenticateToken.js
@@ -1,26 +1,26 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccount.json');
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./serviceAccount.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://phone-book-fe436.firebaseio.com',
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: 'https://phone-book-fe436.firebaseio.com',
+// });
 
-async function decodeIDToken(req, res, next) {
-  const header = req.headers?.authorization;
-  if (header !== 'Bearer null' && req.headers?.authorization?.startsWith('Bearer ')) {
+// async function decodeIDToken(req, res, next) {
+//   const header = req.headers?.authorization;
+//   if (header !== 'Bearer null' && req.headers?.authorization?.startsWith('Bearer ')) {
 
-    const idToken = req.headers.authorization.split('Bearer ')[1];
+//     const idToken = req.headers.authorization.split('Bearer ')[1];
 
-    try {
-      const decodedToken = await admin.auth().verifyIdToken(idToken);
-      req['currentUser'] = decodedToken;
-    } catch (err) {
-      console.log(err);
-    }
-  }
+//     try {
+//       const decodedToken = await admin.auth().verifyIdToken(idToken);
+//       req['currentUser'] = decodedToken;
+//     } catch (err) {
+//       console.log(err);
+//     }
+//   }
 
-  next();
-}
+//   next();
+// }
 
-module.exports = decodeIDToken;
+// module.exports = decodeIDToken;
